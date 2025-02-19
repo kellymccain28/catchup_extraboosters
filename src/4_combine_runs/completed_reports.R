e5e3c8c9f7b14108efec70c1a74be3a94d288ef3
Kelly McCain <kem22@dide.local>
2025-02-19 19:49:37 +0000

    add date filter to make sure all task parameters show up in final df

--- src/4_combine_runs/completed_reports.R
+++ src/4_combine_runs/completed_reports.R
@@ -1,17 +1,22 @@
 #' Return a list of the reports that have already completed from orderly metadata
 #' @param report_name  name of orderly report to extract metadata from
 #' @export
-completed_reports<- function(report_name){
+completed_reports<- function(report_name, datefilter = 20250211000000){
   
   
-  meta <- orderly2::orderly_metadata_extract(name = report_name, extract = c('time', 'parameters'), 
-                                             options = orderly2::orderly_search_options(allow_remote = TRUE))
+  meta <- orderly2::orderly_metadata_extract(name = report_name, extract = c('time', 'parameters')#, 
+                                             # options = orderly2::orderly_search_options(allow_remote = TRUE)
+                                             )
   
   meta<- meta |>
     dplyr::mutate(directory_name = id) |>
     tidyr::separate(col = id, into = c('date', 'time'), sep = '-')|>
     dplyr::mutate(date= as.numeric(date)) |>
     dplyr::mutate(date_time = as.numeric(paste0(date, time)))
+  
+  # Get most recent runs 
+  meta <- meta |>
+    filter(date_time > datefilter)
   
   meta<- data.table(meta)
   meta<- meta[, index:= c(1:.N) ]
